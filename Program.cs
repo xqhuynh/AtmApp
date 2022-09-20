7e13dee956665eeab3d362c51938bc24449787d6
Savon Huynh <xqhuynh@gmail.com>
2022-09-20 15:41:27 -0700

    Set up program w/getters

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class cardHolder {
+
+    string cardNum;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
+
+    // initialize constructor
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance) {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName
+        this.balance = balance;
+    }
+
+    // getters
+    public string getNum() {
+        return cardNum;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public string getFirstName() {
+        return firstName;
+    }
+
+    public string getLastName() {
+        return lastName;
+    }
+
+    public string getBalance() {
+        return balance;
+    }
+
+}
+
